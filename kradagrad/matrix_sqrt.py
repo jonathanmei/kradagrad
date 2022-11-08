c3ed062925c128db36837de0f1b3812548d037c5
Jonathan Mei <jonathan.mei@lmns.com>
2022-11-08 09:59:54 -0800

    tighter norms

--- kradagrad/matrix_sqrt.py
+++ kradagrad/matrix_sqrt.py
@@ -2,15 +2,22 @@ import sys
 
 import torch
 
-def _matrices_info(A: torch.Tensor):
+def _matrices_info(A: torch.Tensor, norm: str='fro'):
     # check if A is 3D, return size and norm
+    # `trace` is True, use trace instead of spectral norm
     A_sz = A.size()
     A_dim = A.dim()
     if A_dim != 3:
         caller = sys._getframe(1).f_code.co_name
         raise ValueError("{} supports 3D Tensors only! Input dim: {}".format(caller, A_dim))
 
-    A_norm = torch.stack([torch.trace(a) for a in A])[:, None, None]
+    if norm == 'tr':
+        A_norm = torch.stack([torch.trace(a) for a in A])
+    elif norm == 'fro':
+        A_norm = torch.stack([torch.linalg.matrix_norm(a) for a in A])
+    else:  # singular value
+        A_norm = (1 + 1e-6) * torch.stack([torch.linalg.matrix_norm(a, ord=2) for a in A])
+    A_norm = A_norm[:, None, None]
     A_dev = A.device
 
     return A_sz, A_dim, A_norm, A_dev
@@ -18,9 +25,9 @@ def _matrices_info(A: torch.Tensor):
 def _batcher(batch_size: int, M: torch.Tensor):
     return M.unsqueeze(0).repeat([batch_size, 1, 1])
 
-def matrix_sqrt_NS(A: torch.Tensor, iters: int=25, batched: bool=False) -> torch.Tensor:
+def matrix_sqrt_NS(A: torch.Tensor, iters: int=25, batched: bool=False, norm: str='fro') -> torch.Tensor:
     # 3D batch of matrices only
-    A_sz, A_dim, A_norm, A_dev = _matrices_info(A)
+    A_sz, A_dim, A_norm, A_dev = _matrices_info(A, norm=norm)
     A_batch = A_sz[0]
 
     Y = A / A_norm
@@ -48,9 +55,9 @@ def matrix_sqrt_NS(A: torch.Tensor, iters: int=25, batched: bool=False) -> torch
     Y = Y * (A_norm ** (0.5))
     return Y
 
-def matrix_sqrt_warm(L: torch.Tensor, L_sqrt_init: torch.Tensor, iters: int=75) -> torch.Tensor:
+def matrix_sqrt_warm(L: torch.Tensor, L_sqrt_init: torch.Tensor, iters: int=100, norm: str='fro') -> torch.Tensor:
     # 3D batch of matrices only
-    L_sz, L_dim, L_norm, L_dev = _matrices_info(L)
+    L_sz, L_dim, L_norm, L_dev = _matrices_info(L, norm=norm)
     L_batch = L_sz[0]
 
     eyes = _batcher(L_batch, torch.eye(*L_sz[-2:], device=L_dev))
