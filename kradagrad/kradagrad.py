0db0e56893b707687e4f840d034cbedcde26b749
Jonathan Mei <jonathan.mei@lmns.com>
2022-11-22 15:57:30 -0800

    include shampoo to compare and extend

--- kradagrad/kradagrad.py
+++ kradagrad/kradagrad.py
@@ -1,5 +1,4 @@
-## Reference code: https://github.com/moskomule/shampoo.pytorch
-## More reference: https://github.com/jettify/pytorch-optimizer/blob/master/torch_optimizer/shampoo.py
+## Simple Kradagrad++ that extends official unoptimized Shampoo implementation
 
 import math
 from typing import Any, Callable, Dict, Iterable, Optional, Tuple, Union
@@ -8,259 +7,66 @@ import torch
 from torch.optim.optimizer import Optimizer
 
 import matrix_root as mr
+from shampoo import (
+    Shampoo, Preconditioner,
+    STEP, MOMENTUM, PRECONDITIONER, GRAFT,
+    LayerwiseGrafting, AdagradGraft, SGDGraft, Graft
+)
 
 
-def roundup(p, m):
-    # how many groups of m are in p?
-    # p: int or float
-    # m: int
-    return (p // m) + int(p % m > 0)
-
-class KradagradPPBatched(Optimizer):
-    r"""Implements KrADagrad++ Optimizer Algorithm, batched statistics updates.
-    Arguments:
-        params: iterable of parameters to optimize or dicts defining
-            parameter groups
-        lr: learning rate (default: 1e-2)
-        momentum: momentum factor (default: 0)
-        weight_decay: weight decay (L2 penalty) (default: 0)
-        epsilon: initialization, for numerical stability of original algo (default: 1e-4)
-        update_freq: number of optimization steps in between updating preconditioner (default: 100)
-        max_size: preconditioner kronecker factor dimension (default: 64)
-        batches: how many preconditioners to update in parallel (default: 8)
-        root: 1/`alpha`, if 1 use ONS-style, if 2 use Adagrad-style (default: 2)
-        inv_tol: error threshhold to terminate inner Newton update on matrix inverse (default: 1e-4)
-        root_tol: error threshhold to terminate outer Newton update on matrix root (default: 1e-4)
-    Example:
-        >>> import kradagrad as kg
-        >>> optimizer = kg.Kradagrad(model.parameters())
-        >>> optimizer.zero_grad()
-        >>> loss_fn(model(input), target).backward()
-        >>> optimizer.step()
-    """
-
-    def __init__(
-        self,
-        params: Union[Iterable[torch.Tensor], Iterable[Dict[str, Any]]],
-        lr: float = 1e-2,
-        momentum: float = 0.0,
-        weight_decay: float = 0.0,
-        epsilon: float = 1e-4,
-        update_freq: int = 100,
-        max_size: int = 64,
-        batches: int = 8,
-        root: int = 2,
-        inv_tol: float = 1e-4,
-        root_tol: float = 1e-4,
-    ):
-
-        if lr <= 0.0:
-            raise ValueError('Invalid learning rate: {} <= 0.0'.format(lr))
-        if momentum < 0.0:
-            raise ValueError('Invalid momentum value: {} < 0.0'.format(momentum))
-        if weight_decay < 0.0:
-            raise ValueError('Invalid weight_decay value: {} < 0.0'.format(weight_decay))
-        if epsilon < 0.0:
-            raise ValueError('Invalid epsilon value: {} < 0.0'.format(epsilon))
-        if update_freq < 1:
-            raise ValueError('Invalid update_freq value: {} < 1'.format(update_freq))
-
-        defaults = dict(
-            lr=lr,
-            momentum=momentum,
-            weight_decay=weight_decay,
-            epsilon=epsilon,
-            update_freq=update_freq,
-            max_size=max_size,
-            batches=batches,
-            root=root,
-            inv_tol=inv_tol,
-            root_tol=root_tol
-        )
-        super(Kradagrad, self).__init__(params, defaults)
-        # Initialize
-        for group in self.param_groups:
-            epsilon = group['epsilon']
-            max_size = group['max_size']
-            root = group['root']
-
-            # # Assume requires_grad doesn't change during training:
-            # param_sizes = [p_.size() for p_ in group['params'] if p_.requires_grad]
-            # def matricize_size(size):
-            #     if len(size) == 1:  # e.g. bias vectors
-            #        mat_sz = (roundup(math.sqrt(size[0]), 1), ) * 2
-            #     elif len(size) > 2:  # higher dim tensors
-            #         split = 
-            #         math.prod()
-            #     return mat_sz
-            # param_sizes = [matricize_size(sz_) for sz_ in param_sizes if len(sz_) != 2]
-            # # break params into chunks of (max_size, max_size), padding along the way
-            # num_blocks_per_param = [
-            #     roundup(sz[0], max_size) * roundup(sz[1], max_size)
-            #     for sz in param_sizes
-            # ]
-            # num_blocks = sum(num_blocks_per_param)
-
-            # Less wasteful but prob won't result in as good a precond matrix:
-            # 1) vectorize and concat all params
-            # 2) make square chunks
-            param_Ns = [p_.numel() for p_ in group['params'] if p_.requires_grad]
-            num_blocks = roundup(sum(param_Ns), max_size * max_size)
-
-            # bookkeeping for where each square chunk comes from
-            param_chunks = []
-            param_num_0 = 0
-            param_ind_0 = 0
-            remaining = 0  # how many params in queue to be chunked
-            for i_ in range(num_blocks):
-                filled = False
-                param_num_1, param_ind_1 = param_num_0, param_ind_0
-                while not filled:
-                    if remaining < max_size * max_size:
-                        remaining += group['params'][param_num_1].numel()
-                        param_num_1 += 1
-                    else:
-                        filled = True
-                        overflow = remaining - max_size * max_size
-                        param_ind_1 = group['params'][param_num_1 - 1].numel() - overflow
-                        remaining = overflow
-                param_chunks.append((param_num_0, param_ind_0), (param_num_1 - 1, param_ind_1))
-                param_num_0, param_ind_1 = param_num_1, param_ind_1
-            
-            self.param_chunks = param_chunks
-            
-            init_mag = 1 / epsilon
-            init_rt_mag = torch.pow(init_mag, 0.5 / root)
-            group_state_init = {}
-
-            group_state_init.update({
-                'L_{}'.format(i_): torch.eye(max_size) * init_mag
-                for i_ in range(num_blocks)
-            })
-            group_state_init.update({
-                'R_{}'.format(i_): torch.eye(max_size) * init_mag
-                for i_ in range(num_blocks)
-            })
-            group_state_init.update({
-                'L_rt_{}'.format(i_): torch.eye(max_size) * init_rt_mag
-                for i_ in range(num_blocks)
-            })
-            group_state_init.update({
-                'R_rt_{}'.format(i_): torch.eye(max_size) * init_rt_mag
-                for i_ in range(num_blocks)
-            })
-
-            self.state[tuple(group['params'])] = group_state_init
-    
+class KrADPreconditioner(Preconditioner):
     @torch.no_grad()
-    def step(self, closure=None) -> Optional[float]:
-        """Performs a single optimization step.
-        Arguments:
-            closure: A closure that reevaluates the model and returns the loss.
+    def add_statistics(self, grad):
+        """Compute inverse KrAD statistics from gradients and add to the correct state entries.
+
+        Args:
+          grad: Gradient to compute statistics from.
         """
-        loss = None
-        if closure is not None:
-            with torch.enable_grad():
-                loss = closure()
-
-        for group in self.param_groups:
-            momentum = group['momentum']
-            weight_decay = group['weight_decay']
-            max_size = group['max_size']
-            
-            # individual gradients
-            for p_ in group['params']:
-                if p_.grad is None:
-                    continue
-                state = self.state[p_]
-                if len(state) == 0:
-                    state['step'] = 0
-                    grad = p_.grad.data
-                    if momentum > 0:
-                        state['momentum_buffer'] = grad.clone()
-                else:
-                    grad = p_.grad.data
-                    if momentum > 0:
-                        grad.mul_(1 - momentum).add_(
-                            state['momentum_buffer'], alpha=momentum
-                        )
-                    if weight_decay > 0:
-                        grad.add_(p_.data, alpha=group['weight_decay'])
-
-            # preconditioners and main loop
-            batches = group['batches']
-            root = group['root']
-            group_state = self.state[tuple(group['params'])]
-            for index_ in range(roundup(len(group_state), batches)):
-                g = _slice_grad(group['params'], batches, max_size, index_)
-                G = g.view([batches, max_size, max_size])
-
-                precond_range = range(max_size * index_, max_size * (index_ + 1))
-                
-                L_s = torch.stack([group_state[key_] for i_ in precon_range if (key_ := 'L_{}'.format(i_)) in group_state])
-                R_s = torch.stack([group_state[key_] for i_ in precon_range if (key_ := 'R_{}'.format(i_)) in group_state])
-                L_rt_s = torch.stack([group_state[key_] for i_ in precon_range if (key_ := 'L_rt_{}'.format(i_)) in group_state])
-                R_rt_s = torch.stack([group_state[key_] for i_ in precon_range if (key_ := 'R_rt_{}'.format(i_)) in group_state])
-
-                M_L = L.bmm(G.bmm(G_T)))
-                t_kL_inv = 1 / (1 + torch.linalg.matrix_norm(M_L, ord='fro'))
-                Delta_L = M_L @ L.transpose(-2, -1)
-
-                M_R = R.bmm(G_T.bmm(G))
-                t_kR_inv = 1 / (1 + torch.linalg.matrix_norm(M_R, ord='fro'))
-                Delta_R = M_R @ R.transpose(-2, -1)
-
-                L_s.sub_(Delta_L, t_kL_inv)
-                R_s.sub_(Delta_R, t_kR_inv)
-                if group_state['step'] % group['update_freq'] == 0:
-                    L_rt_s.copy_(mr.matrix_even_root_warm(2 * root, L_s, L_rt_s))
-                    R_rt_s.copy_(mr.matrix_even_root_warm(2 * root, R_s, R_rt_s))
-
-                grad = L_rt_s.bmm(G.bmm(R_rt_s)).view([-1])
-                _apply_grad(group['params'], grad, lr, batches, max_size, index_)
-
-            state['step'] += 1
-
-        return loss
+        if not self.statistics: return
+        reshaped_grad = torch.reshape(grad, self._transformed_shape)
+        partitioned_grads = self._partitioner.partition(reshaped_grad)
+        w1 = self._hps.beta2
+        w2 = 1.0 if w1 == 1.0 else (1.0 - w1)
+        rank = len(self._transformed_shape)
+        for j, grad in enumerate(partitioned_grads):
+            for i in range(rank):
+                axes = list(range(i)) + list(range(i + 1, rank))
+                stat = torch.tensordot(grad, grad, [axes, axes])
+                stat = stat.mm(self.statistics[j*rank + i].T)
+                t_k_inv = 1.0 / (1.0 + torch.linalg.matrix_norm(stat, 'fro'))
+                stat = self.statistics[j*rank + i].mm(stat)
+                stat.mul_(t_k_inv)
+                self.statistics[j*rank + i].mul_(w1).add_(stat, alpha=w2)
 
     @torch.no_grad()
-    def _slice_grad(self, param_list, batches, max_size, index):
-        N = batches * max_size * max_size
-        chunk = torch.Tensor.zeros([N])
+    def compute_preconditioners(self):
+        """Compute L^{1/exp} for each statistics matrix L"""
+        exp = self.exponent_for_preconditioner()
+        eps = self._hps.matrix_eps
+        for i, stat in enumerate(self.statistics):
+            self.preconditioners[i] = mr.matrix_even_root_N_warm(
+                exp, stat,
+                self.preconditioners[i][None, ...]
+            )[0]  # mr operates on batches
 
-        chunk_beg, chunk_end = self.param_chunks[index]
-        param_num_end, param_ind_end = chunk_end
-        param_num, param_ind = chunk_beg
-        chunk_pointer = 0
-        while chunk_pointer < N:
-            if param_num < param_num_end:
-                addition_size = param_list[param_num].numel() - param_ind
-            else:
-                addition_size = param_ind_end - param_ind
-            chunk[chunk_pointer:chunk_pointer + addition_size] = param_list[param_num].view([-1])[param_ind:param_ind + addition_size]
-            chunk_pointer += addition_size
-            param_num += 1
-            param_ind = 0
-        return chunk.view([batches, max_size, max_size])
+
+class KradagradPP(Shampoo):
+    r"""Implements a simple version of Kradagrad++ Optimizer Algorithm.
+    Extends the unoptimized official pytorch implementation of Shampoo
+    """
+    def init_var_state(self, var, state):
+    """Initialize the PyTorch state of for a single variable."""
+    state[STEP] = 0
+    state[MOMENTUM] = torch.zeros_like(var.data, device=var.get_device())
+    state[PRECONDITIONER] = KrADPreconditioner(var, self.hps)
+    if self.hps.graft_type == LayerwiseGrafting.ADAGRAD:
+      state[GRAFT] = AdagradGraft(self.hps, var)
+    elif self.hps.graft_type == LayerwiseGrafting.SGD:
+      state[GRAFT] = SGDGraft(self.hps, var)
+    else:
+      state[GRAFT] = Graft(self.hps, var)
 
     @torch.no_grad()
-    def _apply_grad(self, param_list, grad, lr, batches, max_size, index):
-        N = batches * max_size * max_size
-        chunk_beg, chunk_end = self.param_chunks[index]
-        param_num_end, param_ind_end = chunk_end
-        param_num, param_ind = chunk_beg
-        chunk_pointer = 0
-        while chunk_pointer < N:
-            this_param = param_list[param_num]
-            if param_num < param_num_end:
-                addition_size = this_param.numel() - param_ind
-            else:
-                addition_size = param_ind_end - param_ind
-            this_param.data.view([-1])[param_ind:param_ind + addition_size].add_(grad[chunk_pointer:chunk_pointer + addition_size], alpha=-lr)
-            if momentum > 0:  # TODO: does this go here or before preconditioning...?
-                state = self.state[this_param]
-                state['momentum_buffer'].view([-1])[param_ind:param_ind + addition_size] = grad[chunk_pointer:chunk_pointer + addition_size]
-            chunk_pointer += addition_size
-            param_num += 1
-            param_ind = 0
+    def step(self, closure=None):
+        super(KradagradPP, self).step(closure=closure)
 
--- kradagrad/matrix_root.py
+++ kradagrad/matrix_root.py
@@ -6,9 +6,8 @@ import torch
 
 import cubic
 
-### Support matrix square roots and odd powered roots separately
-# Allows KrADagrad on generic tensor parameters (e.g. matrices, conv filter banks)
-
+### Support matrix even and odd roots separately
+# Note: most of these are batched, intended for Tensors sized (B, N, N)
 
 def _matrices_info(A: torch.Tensor, norm: str='fro'):
     # check if A is 3D, return size and norm
@@ -33,94 +32,6 @@ def _matrices_info(A: torch.Tensor, norm: str='fro'):
 def _batcher(batch_size: int, M: torch.Tensor):
     return M.unsqueeze(0).repeat([batch_size, 1, 1])
 
-def matrix_sqrt_NS(A: torch.Tensor, iters: int=25, batched: bool=False, norm: str='fro') -> torch.Tensor:
-    # 3D batch of matrices only
-    A_sz, A_dim, A_norm, A_dev = _matrices_info(A, norm=norm)
-    A_batch = A_sz[0]
-
-    Y = A / A_norm
-    Z = torch.eye(*A_sz[-2:], device=A_dev)
-    eye3 = 3 * torch.eye(*A_sz[-2:], device=A_dev)
-
-    # deal with batch dimension:
-    Z = _batcher(A_batch, Z)
-    eye3 = _batcher(A_batch, eye3)
-
-    for it_ in range(iters):
-        X = 0.5 * (eye3 - Z.bmm(Y))
-        if it_ < iters - 1:
-            if batched:  # if we have extra memory
-                U = torch.cat([Y, X])
-                V = torch.cat([X, Z])
-                W = U.bmm(V)
-                Y = W[:A_batch]
-                Z = W[A_batch:]
-            else:
-                Y = Y.bmm(X)
-                Z = X.bmm(Z)
-        else:
-            Y = Y.bmm(X)
-    Y = Y * (A_norm ** (0.5))
-    return Y
-
-def matrix_sqrt_warm(L: torch.Tensor, L_sqrt_init: torch.Tensor, iters: int=100, norm: str='fro', accel: str='line') -> torch.Tensor:
-    # 3D batch of matrices only
-    # nb. naive applications of aitken delta acceleration don't work
-    L_sz, L_dim, L_norm, L_dev = _matrices_info(L, norm=norm)
-    L_batch = L_sz[0]
-
-    eyes = _batcher(L_batch, torch.eye(*L_sz[-2:], device=L_dev))
-    line_search = accel == 'line'
-
-    A = eyes - L / L_norm
-    L_norm_sqrt = L_norm ** 0.5
-    X = eyes - L_sqrt_init.to(L_dev) / L_norm_sqrt
-    for it_ in range(iters):
-        if line_search:
-
-            # solves:
-            #  t = argmin_t || [I - (X + t*D)]^2 - (I - A) ||_F^2
-            #    = argmin_t || C + L*t + Q*t^2 ||_F^2
-
-            X1 = (A + X.bmm(X)) / 2
-            Del = X1 - X
-            XmI = X - eyes
-            Del_norm = torch.linalg.matrix_norm(Del, ord=2)
-            
-            # this one arises from cancellation, not from t
-            # so assign this before normalizing:
-            con = 2 * Del  # (C)onstant
-
-            # for numerical stability of the cubic solver
-            Del = Del / Del_norm[..., None, None]
-
-            qua =  Del.bmm(Del)  # (Q)uadratic
-            lin = Del.bmm(XmI)  # (L)inear
-            lin = lin + lin.transpose(-2, -1)
-
-            # compute cubic coeffs
-            a = 2 * (qua * qua).sum([-2, -1])
-            b = 3 * (qua * lin).sum([-2, -1])
-            c = 2 * (qua * con).sum([-2, -1]) + (lin * lin).sum([-2, -1])
-            d = (lin * con).sum([-2, -1])
-            try:
-                t = cubic.solve_smallest(a, b, c, d, thr=Del_norm)
-
-                # we know the update can be at least Del_norm (unaccelerated)
-                low = (t < Del_norm)
-                t[low] = Del_norm[low]
-                # trust region, keep || X ||_2 < 1
-                max_norm = torch.fmax(1 - torch.linalg.matrix_norm(X, ord=2), Del_norm)
-                t[t > max_norm] = max_norm[t > max_norm]
-
-                # Finally, apply update
-                X = X + t[..., None, None] * Del
-            except:
-                X = X1
-        else:
-            X = (A + X.bmm(X)) / 2
-    return (eyes - X) * L_norm_sqrt
-
 def mat_pow(A: torch.Tensor, p: int):
     # performs A^p using O(log_2(p)) matmuls
     if p < 1:
@@ -141,6 +52,7 @@ def mat_pow(A: torch.Tensor, p: int):
             # stays more symmetric:
             X_prev = X_prev.bmm(X_prev.transpose(-2, -1))
     return Xpow
+
 
 def matrix_inv_warm(A: torch.Tensor, A_p:torch.Tensor, tol: float=1e-4, iters: int=10, norm: str='fro') -> torch.Tensor:
     # Newton method for inverting A
@@ -208,10 +120,100 @@ def matrix_even_root_N_warm(p: int, A: torch.Tensor, A_p: torch.Tensor, tol: flo
             M = M_
     return X * A_norm_p
 
-## Not great compared to Newton or Binomial w/ line search
+def matrix_sqrt_NS(A: torch.Tensor, iters: int=25, batched: bool=False, norm: str='fro') -> torch.Tensor:
+    # 3D batch of matrices only
+    # Newton Schulz iterations; converge quadratically, but no warm start
+    A_sz, A_dim, A_norm, A_dev = _matrices_info(A, norm=norm)
+    A_batch = A_sz[0]
+
+    Y = A / A_norm
+    Z = torch.eye(*A_sz[-2:], device=A_dev)
+    eye3 = 3 * torch.eye(*A_sz[-2:], device=A_dev)
+
+    # deal with batch dimension:
+    Z = _batcher(A_batch, Z)
+    eye3 = _batcher(A_batch, eye3)
+
+    for it_ in range(iters):
+        X = 0.5 * (eye3 - Z.bmm(Y))
+        if it_ < iters - 1:
+            if batched:  # if we have extra memory
+                U = torch.cat([Y, X])
+                V = torch.cat([X, Z])
+                W = U.bmm(V)
+                Y = W[:A_batch]
+                Z = W[A_batch:]
+            else:
+                Y = Y.bmm(X)
+                Z = X.bmm(Z)
+        else:
+            Y = Y.bmm(X)
+    Y = Y * (A_norm ** (0.5))
+    return Y
+
+def matrix_sqrt_warm(L: torch.Tensor, L_sqrt_init: torch.Tensor, iters: int=100, norm: str='fro', accel: str='line') -> torch.Tensor:
+    # 3D batch of matrices only
+    # Binomial iterations. Linear convergence, debatable whether it's comparable even to NS without warm start
+    # nb. naive applications of aitken delta acceleration don't work
+    L_sz, L_dim, L_norm, L_dev = _matrices_info(L, norm=norm)
+    L_batch = L_sz[0]
+
+    eyes = _batcher(L_batch, torch.eye(*L_sz[-2:], device=L_dev))
+    line_search = accel == 'line'
+
+    A = eyes - L / L_norm
+    L_norm_sqrt = L_norm ** 0.5
+    X = eyes - L_sqrt_init.to(L_dev) / L_norm_sqrt
+    for it_ in range(iters):
+        if line_search:
+
+            # solves:
+            #  t = argmin_t || [I - (X + t*D)]^2 - (I - A) ||_F^2
+            #    = argmin_t || C + L*t + Q*t^2 ||_F^2
+
+            X1 = (A + X.bmm(X)) / 2
+            Del = X1 - X
+            XmI = X - eyes
+            Del_norm = torch.linalg.matrix_norm(Del, ord=2)
+            
+            # this one arises from cancellation, not from t
+            # so assign this before normalizing:
+            con = 2 * Del  # (C)onstant
+
+            # for numerical stability of the cubic solver
+            Del = Del / Del_norm[..., None, None]
+
+            qua =  Del.bmm(Del)  # (Q)uadratic
+            lin = Del.bmm(XmI)  # (L)inear
+            lin = lin + lin.transpose(-2, -1)
+
+            # compute cubic coeffs
+            a = 2 * (qua * qua).sum([-2, -1])
+            b = 3 * (qua * lin).sum([-2, -1])
+            c = 2 * (qua * con).sum([-2, -1]) + (lin * lin).sum([-2, -1])
+            d = (lin * con).sum([-2, -1])
+            try:
+                t = cubic.solve_smallest(a, b, c, d, thr=Del_norm)
+
+                # we know the update can be at least Del_norm (unaccelerated)
+                low = (t < Del_norm)
+                t[low] = Del_norm[low]
+                # trust region, keep || X ||_2 < 1
+                max_norm = torch.fmax(1 - torch.linalg.matrix_norm(X, ord=2), Del_norm)
+                t[t > max_norm] = max_norm[t > max_norm]
+
+                # Finally, apply update
+                X = X + t[..., None, None] * Del
+            except:
+                X = X1
+        else:
+            X = (A + X.bmm(X)) / 2
+    return (eyes - X) * L_norm_sqrt
+
 def make_pade_rooter(n: int, m: int, l: int, norm: str='fro'):
     # Uses pade approximation of order[m, l] to create a function
     # that computes the nth root on a batch of matrices
+    # Nb. Not great compared to Newton or even Binomial w/ line search
     f = lambda x: mpmath.root(1 - x, n)
     a = mpmath.taylor(f, 0, m+l+4)
     p, q = mpmath.pade(a, m, l)
