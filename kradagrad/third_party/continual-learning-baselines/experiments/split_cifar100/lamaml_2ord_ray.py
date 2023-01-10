c3b5251a130cc0bc38b25aa8bce71d6540d88b9e
Luke Walters <luke.walters@lmns.com>
2023-01-10 23:43:22 +0000

    lamaml experiment

--- kradagrad/third_party/continual-learning-baselines/experiments/split_cifar100/lamaml_2ord_ray.py
+++ kradagrad/third_party/continual-learning-baselines/experiments/split_cifar100/lamaml_2ord_ray.py
@@ -1,17 +1,77 @@
+import os
+import sys
+
 import avalanche as avl
 import torch
-from torch.nn import CrossEntropyLoss
-
 from avalanche.evaluation import metrics as metrics
-from avalanche.training.storage_policy import ReservoirSamplingBuffer
 from avalanche.training.plugins import ReplayPlugin
+from avalanche.training.storage_policy import ReservoirSamplingBuffer
 from avalanche.training.supervised.lamaml import LaMAML
-
+from experiments.utils import create_default_args, set_seed
 from models.models_lamaml import MTConvCIFAR
-from experiments.utils import set_seed, create_default_args
+from ray import tune
+from ray.air import session
+from ray.air.checkpoint import Checkpoint
+from torch.nn import CrossEntropyLoss
+from torch.optim import SGD, Adam
+
+sys.path.append(os.path.expanduser("~/"))
+
+import kradagrad
 
 
-def lamaml_scifar100(override_args=None):
+def get_optimizer(
+    params,
+    optimizer="sgd",
+    block_size=128,
+    weight_decay=0.0,
+    inverse_exponent_override=0,
+    eps=1e-6,
+    lr=0.01,
+    momentum=0.9,
+    debug=False,
+    precon_update_freq=20,
+):
+    hps = kradagrad.HyperParams(
+        matrix_eps=eps,
+        weight_decay=weight_decay,
+        graft_type=0,
+        beta2=1,
+        block_size=block_size,
+        best_effort_shape_interpretation=True,
+        inverse_exponent_override=inverse_exponent_override,
+        preconditioning_compute_steps=precon_update_freq,
+    )
+    if optimizer == "shampoo":
+        optimizer = kradagrad.Shampoo(params, lr=lr, hyperparams=hps, momentum=momentum)
+    elif optimizer == "kradmm":
+        optimizer = kradagrad.KradagradMM(
+            params,
+            lr=lr,
+            hyperparams=hps,
+            momentum=momentum,
+            debug=debug,
+        )
+    elif optimizer == "krad":
+        optimizer = kradagrad.KradagradPP(
+            params,
+            lr=lr,
+            momentum=momentum,
+            hyperparams=hps,
+            tensor_batch_size=0,
+        )  # tensor batching only for single run
+
+    if optimizer == "sgd":
+        optimizer = torch.optim.SGD(
+            params, lr, weight_decay=weight_decay, momentum=momentum
+        )
+    elif optimizer == "adam":
+        optimizer = torch.optim.Adam(params, lr, weight_decay=weight_decay, eps=eps)
+
+    return optimizer
+
+
+def lamaml_scifar100(config):
     """
     "La-MAML: Look-ahead Meta Learning for Continual Learning",
     Gunshi Gupta, Karmesh Yadav, Liam Paull;
@@ -20,26 +80,40 @@ def lamaml_scifar100(override_args=None):
     """
     # Args
     args = create_default_args(
-        {'cuda': 0, 'n_inner_updates': 5, 'second_order': True,
-         'grad_clip_norm': 1.0, 'learn_lr': True, 'lr_alpha': 0.25,
-         'sync_update': False, 'mem_size': 200, 'lr': 0.1,
-         'train_mb_size': 10, 'train_epochs': 10, 'seed': None}, override_args
+        {
+            "cuda": 0,
+            "n_inner_updates": 5,
+            "second_order": True,
+            "grad_clip_norm": 1.0,
+            "learn_lr": True,
+            "lr_alpha": 0.25,
+            "sync_update": False,
+            "mem_size": 200,
+            "lr": 0.1,
+            "train_mb_size": 10,
+            "train_epochs": 10,
+            "seed": None,
+            "optimizer": "sgd",
+            "eps": 1e-6,
+        },
+        config,
     )
 
     set_seed(args.seed)
-    device = torch.device(f"cuda:{args.cuda}"
-                          if torch.cuda.is_available() and
-                          args.cuda >= 0 else "cpu")
+    device = torch.device(
+        f"cuda:{args.cuda}" if torch.cuda.is_available() and args.cuda >= 0 else "cpu"
+    )
     # Benchmark
-    benchmark = avl.benchmarks.SplitCIFAR100(n_experiences=20,
-                                             return_task_id=True)
+    benchmark = avl.benchmarks.SplitCIFAR100(n_experiences=20, return_task_id=True)
 
     # Loggers and metrics
     interactive_logger = avl.logging.InteractiveLogger()
+    tb_logger = avl.logging.TensorboardLogger(tb_log_dir=f"tb")
 
     evaluation_plugin = avl.training.plugins.EvaluationPlugin(
         metrics.accuracy_metrics(epoch=True, experience=True, stream=True),
-        loggers=[interactive_logger])
+        loggers=[interactive_logger, tb_logger],
+    )
 
     # Buffer
     rs_buffer = ReservoirSamplingBuffer(max_size=args.mem_size)
@@ -48,11 +122,19 @@ def lamaml_scifar100(override_args=None):
         batch_size=args.train_mb_size,
         batch_size_mem=args.train_mb_size,
         task_balanced_dataloader=False,
-        storage_policy=rs_buffer
+        storage_policy=rs_buffer,
     )
 
     # Strategy
     model = MTConvCIFAR()
+
+    optimizer = get_optimizer(
+        model.parameters(),
+        optimizer=args.optimizer,
+        lr=args.lr,
+        eps=args.eps,
+    )
+
     cl_strategy = LaMAML(
         model,
         torch.optim.SGD(model.parameters(), lr=args.lr),
@@ -78,6 +160,31 @@ def lamaml_scifar100(override_args=None):
     return res
 
 
-if __name__ == '__main__':
-    res = lamaml_scifar100()
-    print(res)
+if __name__ == "__main__":
+    config_2ord = {
+        "lr": tune.grid_search([1, 2.5e-1, 1e-1, 2.5e-2, 1e-2]),
+        "optimizer": tune.grid_search(["shampoo", "krad", "kradmm"]),
+        "seed": tune.grid_search([100, 200, 300]),
+        "eps": tune.grid_search([1e-4]),
+    }
+    config_1ord = {
+        "lr": tune.grid_search([1e-1, 5e-2, 2e-2, 1e-2, 5e-3, 2e-3, 1e-3]),
+        "optimizer": tune.grid_search(["sgd", "adam"]),
+        "seed": tune.grid_search([100, 200, 300]),
+        "eps": tune.grid_search([1e-6]),
+    }
+
+    trainer = tune.with_resources(
+        tune.with_parameters(lamaml_scifar100), resources={"cpu": 1, "gpu": 1.0 / 12.0}
+    )
+
+    tune_config = tune.TuneConfig(
+        metric="Top1_Acc_Stream/eval_phase/test_stream/Task019",
+        mode="max",
+        num_samples=1,
+    )
+    tuner = tune.Tuner(trainer, param_space=config_1ord, tune_config=tune_config)
+    results_1ord = tuner.fit()
+
+    tuner = tune.Tuner(trainer, param_space=config_2ord, tune_config=tune_config)
+    results_2ord = tuner.fit()
